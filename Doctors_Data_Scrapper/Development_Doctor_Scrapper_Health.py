57663385021da9c44ba75cbefe4f755089bc91d2
ammansoomro <amman.soomro.as@gmail.com>
2022-08-31 07:45:46 +0500

    Updated Scrappers

--- Doctors_Data_Scrapper/Development_Doctor_Scrapper_Health.py
+++ Doctors_Data_Scrapper/Development_Doctor_Scrapper_Health.py
@@ -1,6 +1,5 @@
 import requests
 import bs4
-from lxml import etree
 
 
 def scrap_for_data(x):
@@ -19,14 +18,48 @@ def scrap_for_data(x):
             "a", {'class': 'summary-standard-toggle-phone-number-button'})
         Gender = soup.find(
             "span", {'data-qa-target': 'ProviderDisplayGender'})
+        Address = soup.find(
+            "address", {'class': 'location-row-address'})
+        State = soup.find(
+            "a", {'class': 'breadcrumb-link link--secondary',
+                  'data-qa-target': 'breadcrumb-city-directory'})
         print("==========================================================")
-        print(Name.getText())
-        print(str(Phone).split(" "))
-        print(str(Phone02).split(" "))
-        print(Phone03)
-        print(Gender.getText())
-        print("==========================================================") 
-        print(" ")
+        print("Name:" + Name.getText())
+        Doctor_Name = "Not Found"
+        Doctor_Name = Name.getText().replace(",", " ").strip("\n")
+        try:
+            Phone_01 = "Not Found"
+            Phone_01 = str(Phone).replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        try:
+            Phone_02 = "Not Found"
+            Phone_02 = str(Phone02).replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        try:
+            Phone_03 = "Not Found"
+            Phone_03 = str(Phone03).replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        try:
+            Doctor_Gender = "Not Found"
+            Doctor_Gender = Gender.getText().replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        try:
+            Doctor_Address = "Not Found"
+            Doctor_Address = Address.getText().replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        try:
+            Doctor_State = "Not Found"
+            Doctor_State = State.getText().replace(",", " ").strip("\n")
+        except Exception as e:
+            print(e)
+        print("==========================================================")
+        output = output + Doctor_Name + "," + Phone_01 + "," + Phone_02 + "," + \
+            Phone_03 + "," + Doctor_Gender + "," + Doctor_Address + "," + Doctor_State + "\n"
     except Exception as e:
         print(e)
 
@@ -34,8 +67,9 @@ def scrap_for_data(x):
 
 
 with open("HealthGrades.txt", "r") as file:
-    with open("Output.csv", "w") as outputfile:
-        outputfile.write("Name\n")
+    Filename = input("Enter File Name")
+    with open(Filename+".csv", "w") as outputfile:
+        outputfile.write("Name,Phone01,Phone02,Phone03,Gender,Address,State\n")
         for i in file.readlines():
             v = scrap_for_data(i.rstrip('\n'))
             try:
