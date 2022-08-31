cd917ace67fddd4f29b6d75f6b10b5b068a72077
ammansoomro <amman.soomro.as@gmail.com>
2022-08-31 07:48:41 +0500

    Updated Scrappers

--- GitScrapper/bot.py
+++
@@ -1,8 +0,0 @@
-from search_engines import Google
-
-engine = Google()
-query = input("Enter Query: ")
-results = engine.search(query)
-links = results.links()
-
-print(links)
--- Search_Bot/Bot.py
+++
@@ -1,7 +0,0 @@
-from search_engines import Google
-
-engine = Google()
-results = engine.search("Lana Rhoades Anal Dp")
-links = results.links()
-
-print(links)
