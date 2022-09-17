03eb970ed33fd43b7b51b2781b917b2f2412db43
Amman Soomro <amman.soomro.as@gmail.com>
2022-09-17 21:59:19 +0500

    New Bots and Updated Files

---
+++ Cards_Scrapper/TCGPlayers/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+            Name = row.contents[3].contents[0].contents[0].a.contents[0]
+            print(Name)
---
+++ Google_MetaData_AllPages_Scrapper/MetaDataScrapper.py
@@ -0,0 +1,86 @@
+import numpy as np
+from requests_html import HTMLSession
+from requests_html import HTMLSession
+
+loss = 0
+
+
+def google_search(query, count):
+    response = get_results(query, count)
+    return parse_results(response, query)
+
+
+def get_results(query, count):
+    response = get_source("https://google.com/search?q=" +
+                          query + "&start=" + str(count))
+    return response
+
+
+def get_source(url):
+    session = HTMLSession()
+    response = session.get(url)
+    return response
+
+
+def parse_results(response, keyword):
+    keyword = "Amazon FBA"
+    try:
+        css_identifier_result = ".tF2Cxc"
+        css_identifier_title = "h3"
+        css_identifier_link = ".yuRUbf a"
+        css_identifier_text = ".VwiC3b"
+        results = response.html.find(css_identifier_result)
+
+        output = ""
+
+        for result in results:
+            print("============================================================")
+            title = (result.find(css_identifier_title)
+                     [0].text).strip().replace(",", "")
+            link = result.find(css_identifier_link)[0].attrs['href']
+            text = (result.find(css_identifier_text)[
+                    0].text).strip().replace(",", "")
+            print("Title: " + title)
+            print("Link: " + link)
+            print("Text: " + text)
+            print("============================================================")
+            searchwords = keyword.lower().split(" ")
+            length = len(searchwords)
+            newEntry = title + "," + link + "," + text + "\n"
+
+            ################### NUMPY TESTING ################################
+            SearchingWords = keyword.lower().split(" ")
+            SearchingQuery = title + " " + link + " " + text
+            SearchingQuery = SearchingQuery.lower().replace(
+                ',', " ").replace('.', " ").replace("-", " ").split(" ")
+            SearchingWords = np.array(SearchingWords)
+            SearchingQuery = np.array(SearchingQuery)
+            if (np.intersect1d(SearchingWords, SearchingQuery).size == SearchingWords.size):
+                output = output + newEntry
+            ################### NUMPY TESTING ################################
+
+    except Exception as e:
+        print("Searcing...")
+
+    return output
+
+
+# OPENING AND WRITING IN FILE
+filename = input("Enter Output filename:")
+with open('Potential_Links.txt', 'r') as f:
+    with open(filename + ".csv", "w", encoding='utf-8') as outputfile:
+        for line in f.readlines():
+            outputfile.write("Title,Link,Text\n")
+            print(line)
+            Keyword = line
+            count = 0
+            while count < 10:
+                Output = google_search(Keyword, count)
+                count += 10
+                if (len(Output) > 1):
+                    outputfile.write(Output)
+                else:
+                    outputfile.write("No Results Found," + Keyword + ",\n")
+
+print("Search Complete")
+# END OF PROGRAM
